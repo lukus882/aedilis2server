01ea9633682defa8e4fb64f6bd20cf660f4f6955
stormwolf <stormwolf@fee03c0e-a727-0410-94d1-4d07d25b926a>
2007-01-23 20:05:15 +0000

    Added a few misc scripts

---
+++ Scripts/Custom/Addons/WoodWorkerBenchEastAddon.cs
@@ -0,0 +1,76 @@
+using System;
+using Server;
+using Server.Items;
+
+namespace Server.Items
+{
+	public class WoodWorkerBenchEastAddon : BaseAddon
+	{
+		public override BaseAddonDeed Deed
+		{
+			get
+			{
+				return new WoodWorkerBenchEastAddonDeed();
+			}
+		}
+
+		[ Constructable ]
+		public WoodWorkerBenchEastAddon()
+		{
+			AddComponent( new AddonComponent( 6642 ), 0, 0, 0 );
+			AddComponent( new AddonComponent( 6641 ), 0, 1, 0 );
+			AddComponent( new AddonComponent( 6643 ), 0, -1, 0 );
+
+		}
+
+		public WoodWorkerBenchEastAddon( Serial serial ) : base( serial )
+		{
+			Name="woodworkers bench";
+		}
+
+		public override void Serialize( GenericWriter writer )
+		{
+			base.Serialize( writer );
+			writer.Write( 0 ); // Version
+		}
+
+		public override void Deserialize( GenericReader reader )
+		{
+			base.Deserialize( reader );
+			int version = reader.ReadInt();
+		}
+	}
+
+	public class WoodWorkerBenchEastAddonDeed : BaseAddonDeed
+	{
+		public override BaseAddon Addon
+		{
+			get
+			{
+				return new WoodWorkerBenchEastAddon();
+			}
+		}
+
+		[Constructable]
+		public WoodWorkerBenchEastAddonDeed()
+		{
+			Name = "woodworkers bench deed (east)";
+		}
+
+		public WoodWorkerBenchEastAddonDeed( Serial serial ) : base( serial )
+		{
+		}
+
+		public override void Serialize( GenericWriter writer )
+		{
+			base.Serialize( writer );
+			writer.Write( 0 ); // Version
+		}
+
+		public override void	Deserialize( GenericReader reader )
+		{
+			base.Deserialize( reader );
+			int version = reader.ReadInt();
+		}
+	}
+}
---
+++ Scripts/Custom/Addons/WoodWorkerBenchSouthAddon.cs
@@ -0,0 +1,76 @@
+using System;
+using Server;
+using Server.Items;
+
+namespace Server.Items
+{
+	public class WoodWorkerBenchSouthAddon : BaseAddon
+	{
+		public override BaseAddonDeed Deed
+		{
+			get
+			{
+				return new WoodWorkerBenchSouthAddonDeed();
+			}
+		}
+
+		[ Constructable ]
+		public WoodWorkerBenchSouthAddon()
+		{
+			AddComponent( new AddonComponent( 6647 ), -1, 0, 0 );
+			AddComponent( new AddonComponent( 6645 ), 1, 0, 0 );
+			AddComponent( new AddonComponent( 6646 ), 0, 0, 0 );
+
+		}
+
+		public WoodWorkerBenchSouthAddon( Serial serial ) : base( serial )
+		{
+			Name="woodworkers bench";
+		}
+
+		public override void Serialize( GenericWriter writer )
+		{
+			base.Serialize( writer );
+			writer.Write( 0 ); // Version
+		}
+
+		public override void Deserialize( GenericReader reader )
+		{
+			base.Deserialize( reader );
+			int version = reader.ReadInt();
+		}
+	}
+
+	public class WoodWorkerBenchSouthAddonDeed : BaseAddonDeed
+	{
+		public override BaseAddon Addon
+		{
+			get
+			{
+				return new WoodWorkerBenchSouthAddon();
+			}
+		}
+
+		[Constructable]
+		public WoodWorkerBenchSouthAddonDeed()
+		{
+			Name = "woodworkers bench deed (south)";
+		}
+
+		public WoodWorkerBenchSouthAddonDeed( Serial serial ) : base( serial )
+		{
+		}
+
+		public override void Serialize( GenericWriter writer )
+		{
+			base.Serialize( writer );
+			writer.Write( 0 ); // Version
+		}
+
+		public override void	Deserialize( GenericReader reader )
+		{
+			base.Deserialize( reader );
+			int version = reader.ReadInt();
+		}
+	}
+}
