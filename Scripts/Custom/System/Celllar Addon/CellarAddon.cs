42b3cd4b9756341170d34cb15f97cb48ed3582a2
stormwolf <stormwolf@fee03c0e-a727-0410-94d1-4d07d25b926a>
2007-01-27 20:32:36 +0000

    Added Cat Witch found here: http://www.runuo.com/forums/custom-script-releases/74469-runuo-2-0-rc1-caith-sidhe.html
    Added Cellar Addon found here:
    http://www.runuo.com/forums/custom-script-releases/71588-runuo-2-0-rc1-cellar-addon.html

---
+++ Scripts/Custom/System/Celllar Addon/CellarAddon.cs
@@ -0,0 +1,231 @@
+using System;
+using Server;
+using Server.Network;
+using System.Collections;
+using Server.Multis;
+
+namespace Server.Items
+{
+	public class CellarAddon : BaseAddon, IChopable
+	{
+		public override BaseAddonDeed Deed{ get{ return new CellarDeed(); } }
+
+		[Constructable]
+		public CellarAddon()
+		{
+                        AddComponent( new AddonComponent( 0x31F4 ), -1, -1, -40 );
+			AddComponent( new AddonComponent( 0x31F4 ),  0, -1, -40 );
+			AddComponent( new AddonComponent( 0x31F4 ),  1, -1, -40 );
+			AddComponent( new AddonComponent( 0x31F4 ), -1,  0, -40 );
+			AddComponent( new AddonComponent( 0x31F4 ),  0,  0, -40 );
+			AddComponent( new AddonComponent( 0x31F4 ),  1,  0, -40 );
+			AddComponent( new AddonComponent( 0x31F4 ), -1,  1, -40 );
+			AddComponent( new AddonComponent( 0x31F4 ),  0,  1, -40 );
+			AddComponent( new AddonComponent( 0x31F4 ),  1,  1, -40 );
+                        AddComponent( new AddonComponent( 0x31F4 ), -2, -2, -40 );
+			AddComponent( new AddonComponent( 0x31F4 ),  0, -2, -40 );
+			AddComponent( new AddonComponent( 0x31F4 ),  2, -2, -40 );
+			AddComponent( new AddonComponent( 0x31F4 ), -2,  0, -40 );
+                        AddComponent( new AddonComponent( 0x31F4 ),  2,  0, -40 );
+			AddComponent( new AddonComponent( 0x31F4 ), -2,  2, -40 );
+			AddComponent( new AddonComponent( 0x31F4 ),  0,  2, -40 );
+			AddComponent( new AddonComponent( 0x31F4 ),  2,  2, -40 );
+                        AddComponent( new AddonComponent( 0x31F4 ), 1, 2, -40 );
+			AddComponent( new AddonComponent( 0x31F4 ), 2, 1, -40 );
+                        AddComponent( new AddonComponent( 0x31F4 ), -1, 2, -40 );
+			AddComponent( new AddonComponent( 0x31F4 ), -2, 1, -40 );
+                        AddComponent( new AddonComponent( 0x31F4 ), 1, -2, -40 );
+			AddComponent( new AddonComponent( 0x31F4 ), 2, -1, -40 );
+                        AddComponent( new AddonComponent( 0x31F4 ), -1, -2, -40 );
+			AddComponent( new AddonComponent( 0x31F4 ), -2, -1, -40 );
+   
+			AddComponent( new AddonComponent( 0x31F4 ), 0, 3, -40 );
+			AddComponent( new AddonComponent( 0x31F4 ), 3, 0, -40 );
+			AddComponent( new AddonComponent( 0x31F4 ), 1, 3, -40 );
+			AddComponent( new AddonComponent( 0x31F4 ), 3, 1, -40 );
+                        AddComponent( new AddonComponent( 0x31F4 ), -1, 3, -40 );
+			AddComponent( new AddonComponent( 0x31F4 ), -3, 1, -40 );
+                        AddComponent( new AddonComponent( 0x31F4 ), 1, -3, -40 );
+			AddComponent( new AddonComponent( 0x31F4 ), 3, -1, -40 );
+                        AddComponent( new AddonComponent( 0x31F4 ), -1, -3, -40 );
+			AddComponent( new AddonComponent( 0x31F4 ), -3, -1, -40 );
+			AddComponent( new AddonComponent( 0x31F4 ), 2, 3, -40 );
+			AddComponent( new AddonComponent( 0x31F4 ), 3, 2, -40 );
+                        AddComponent( new AddonComponent( 0x31F4 ), -2, 3, -40 );
+			AddComponent( new AddonComponent( 0x31F4 ), -3, 2, -40 );
+                        AddComponent( new AddonComponent( 0x31F4 ), 2, -3, -40 );
+			AddComponent( new AddonComponent( 0x31F4 ), 3, -2, -40 );
+                        AddComponent( new AddonComponent( 0x31F4 ), -2, -3, -40 );
+			AddComponent( new AddonComponent( 0x31F4 ), -3, -2, -40 );
+                        AddComponent( new AddonComponent( 0x31F4 ), -3, -3, -40 );
+			AddComponent( new AddonComponent( 0x31F4 ), -3, 3, -40 );
+                        AddComponent( new AddonComponent( 0x31F4 ), 3, -3, -40 );
+			AddComponent( new AddonComponent( 0x31F4 ), 3, 3, -40 );
+                        AddComponent( new AddonComponent( 0x31F4 ), 0, -3, -40 );
+			AddComponent( new AddonComponent( 0x31F4 ), -3, 0, -40 );
+   
+                        AddComponent( new CellarTeleporter(), 0, 0, -39 );
+                        AddComponent( new CellarTeleporter1(), 0, 0, 0 );
+		}
+
+		public CellarAddon( Serial serial ) : base( serial )
+		{
+		}
+
+		public override void Serialize( GenericWriter writer )
+		{
+			base.Serialize( writer );
+
+			writer.Write( (int) 0 ); // version
+		}
+
+		public override void Deserialize( GenericReader reader )
+		{
+			base.Deserialize( reader );
+
+			int version = reader.ReadInt();
+		}
+	}
+
+	public class CellarDeed : BaseAddonDeed
+	{
+		public override BaseAddon Addon{ get{ return new CellarAddon(); } }
+	
+
+		[Constructable]
+		public CellarDeed()
+		{
+                 Name = "Cellar Deed";
+              	 LootType = LootType.Blessed;
+		 Weight = 5.0;
+                 Hue = 46;
+		}
+
+		public override void OnDoubleClick( Mobile from )
+		{
+			if (from.AccessLevel < AccessLevel.GameMaster)
+			{
+				from.SendMessage( "You require a Game Master's Help In Placing This Addon" );
+				return;
+			}
+
+			else if ( from.Z <= -20 )
+			{
+				from.SendMessage( "You can not make a cellar here - you are to deep" );
+				return;
+			}
+		
+			base.OnDoubleClick( from );
+				
+		}
+
+		public CellarDeed( Serial serial ) : base( serial )
+		{
+		}
+
+		public override void Serialize( GenericWriter writer )
+		{
+			base.Serialize( writer );
+
+			writer.Write( (int) 0 ); // version
+		}
+
+		public override void Deserialize( GenericReader reader )
+		{
+			base.Deserialize( reader );
+
+			int version = reader.ReadInt();
+		}
+	}
+ 
+        public class CellarTeleporter1 : AddonComponent
+	{
+		[Constructable]
+		public CellarTeleporter1() : this( false )
+		{
+		}
+
+		[Constructable]
+		public CellarTeleporter1( bool creatures ) : base( 0xA97 )
+		{
+			Movable = false;
+			Visible = true;
+		}
+
+                public override void OnDoubleClick( Mobile from )
+		{
+
+			if (!from.InRange( this.GetWorldLocation(), 1 ) )
+			{
+                               from.LocalOverheadMessage( MessageType.Regular, 0x3B2, 1019045 ); // I can't reach that.
+			}
+			else
+			{
+				from.Z -= 40;
+			}
+		}
+
+		public CellarTeleporter1( Serial serial ) : base( serial )
+		{
+		}
+
+		public override void Serialize( GenericWriter writer )
+		{
+			base.Serialize( writer );
+
+			writer.Write( (int) 0 ); // version
+
+		}
+
+		public override void Deserialize( GenericReader reader )
+		{
+			base.Deserialize( reader );
+
+			int version = reader.ReadInt();
+
+		}
+	}
+	public class CellarTeleporter : AddonComponent
+	{
+
+		[Constructable]
+		public CellarTeleporter() : this( false )
+		{
+		}
+
+		[Constructable]
+		public CellarTeleporter( bool creatures ) : base( 0x71F )
+		{
+			Movable = false;
+			Visible = true;
+                }
+                
+                public override bool OnMoveOver( Mobile m )
+		{
+                       {
+                        m.Z += 40;
+                        
+                        return false;
+                        }
+                   return true;
+                 }
+                 
+		public CellarTeleporter( Serial serial ) : base( serial )
+		{
+		}
+
+		public override void Serialize( GenericWriter writer )
+		{
+			base.Serialize( writer );
+			writer.Write( (int) 0 ); // version
+		}
+
+		public override void Deserialize( GenericReader reader )
+		{
+			base.Deserialize( reader );
+
+			int version = reader.ReadInt();
+
+		}
+	}
+}
