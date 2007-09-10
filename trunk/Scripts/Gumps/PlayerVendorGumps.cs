bff13f3c17c0dac1c3528a764fc507a8e1a271de
stormwolf <stormwolf@fee03c0e-a727-0410-94d1-4d07d25b926a>
2007-09-10 14:38:16 +0000

    Updated to latest RunUO Exe SVN 237 Bugfix in player vendor Gumps/PlayerVendorGumps.cs

--- trunk/Scripts/Gumps/PlayerVendorGumps.cs
+++ trunk/Scripts/Gumps/PlayerVendorGumps.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Reflection;
@@ -55,7 +55,7 @@ namespace Server.Gumps
 
 			if ( info.ButtonID == 1 )
 			{
-				if ( !m_VI.Valid )
+				if ( !m_VI.Valid || !m_VI.Item.IsChildOf( m_Vendor.Backpack ) )
 				{
 					m_Vendor.SayTo( from, 503216 ); // You can't buy that.
 					return;
